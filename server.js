551321356fe8bb73266ec79a5967ded41de85b72
Aditi Nair Balachandran <71278983+AditiNairB@users.noreply.github.com>
2020-10-04 20:17:49 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,32 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const data = require('./budget-data.json')
 
 app.use('/', express.static('public'));
-
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
   });
 
   app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.json(data);
 });
   
   app.listen(port, () => {
