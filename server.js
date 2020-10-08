f5b6ab0a7f0ea20ffdfa37c1dcb2c16325160cd3
Aditi Nair Balachandran <71278983+AditiNairB@users.noreply.github.com>
2020-10-08 13:04:05 -0400

    added cors

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
+//Budget API
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const data = require('./budget-data.json')
 
-app.use('/', express.static('public'));
+// app.use('/', express.static('public'));
+app.use(cors());
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-  });
+// app.get('/hello', (req, res) => {
+//     res.send('Hello World!');
+//   });
 
   app.get('/budget', (req, res) => {
     res.json(data);
 });
   
   app.listen(port, () => {
-    console.log(`Serving at http://localhost:${port}`)
+    console.log(`API Serving at http://localhost:${port}`)
   });
 
     
